290008653a2c46a2cb59ddd33d76906cb9b3e772
Topias <topias.pyykkonen@gmail.com>
2017-03-15 18:20:19 +0200

    createMap-function

---
+++ projects/project3/createMap.R
@@ -0,0 +1,18 @@
+
+
+
+setwd('C:\\Users\\user1\\r_crap\\')
+
+
+
+
+
+createMap <- function(data='example') {
+  source("https://topiasp.github.io/projects/project3/create_JS_objects.R")
+  writeLines(createJSobjects(data),'data.js')
+  writeLines(readLines("https://topiasp.github.io/projects/project3/map.html",warn=F),con='map.html')
+  writeLines(readLines("https://topiasp.github.io/projects/project3/funktiot.js",warn=F),con='funktiot.js')
+  writeLines(readLines("https://topiasp.github.io/projects/project3/styles.css",warn=F),con='styles.css')
+}
+
+createMap()
