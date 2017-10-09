1f4ae0f1a17b28b65f0798eb9e4bcaf2a75495ad
topiasp <topias.pyykkonen@gmail.com>
2017-10-09 11:27:46 +0300

    update

--- projects/toimeentulotuki/JS/functions.js
+++ projects/toimeentulotuki/JS/functions.js
@@ -3,9 +3,12 @@ function getSeries(indicatorID, cbFunction= function() {  console.log('no cb') }
 
 
 	// 405 : "fi": "Toimeentulotukea saaneet 18 - 24-vuotiaat, % vastaavanikäisestä väestöstä"
-	urli ='http://www.sotkanet.fi/rest/1.1/json?indicator=' + indicatorID +'&genders=total&years=2016';
-
+	// urli ='http://www.sotkanet.fi/rest/1.1/json?indicator=' + indicatorID +'&genders=total&years=2016';
+    // 
+	urli = 'http://www.sotkanet.fi/rest/1.1/json?indicator=127&years=2009&genders=total'
 	var xhttp = new XMLHttpRequest();
+	
+	
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 		   
@@ -15,6 +18,8 @@ function getSeries(indicatorID, cbFunction= function() {  console.log('no cb') }
 		}
 	};
 	xhttp.open("GET", urli, true);
+	xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+	xhttp.setRequestHeader( 'Content-Type', 'application/json' );
 	xhttp.send();
 
 };
