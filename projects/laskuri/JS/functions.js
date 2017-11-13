7944f162c5d6152522499d93eaab9f574fadadec
topias <topias.pyykkonen@gmail.com>
2017-11-13 22:08:12 +0200

    Laskurin lisäys

---
+++ projects/laskuri/JS/functions.js
@@ -0,0 +1,40 @@
+
+var globalHelp;
+var globalHelp2;
+
+
+
+$(document).ready(function() {
+	
+    console.log( "ready!" );
+		
+		
+	
+	obj = $("<div value='0'><p>AAA</p></div>").addClass('count');
+	
+	$('.container').append(obj)
+	
+	
+		
+		
+	obj = $("<div id='pigButton'><img src='pigButton.png'></img></div>").addClass('animalButton').on('click',function() {
+		
+		console.log('click!');
+		$(this).css('width',300).css('height',300);
+		
+		prevValue = $('.count').attr('value')*1;
+		
+		$('.count').attr('value',prevValue+1);
+		
+		$('.count p')[0].innerHTML = $('.count').attr('value')
+		
+		
+	});
+	$('.container').append(obj);
+	
+	
+	
+	
+	
+	
+});
