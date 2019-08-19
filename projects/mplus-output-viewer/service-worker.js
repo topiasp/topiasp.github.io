bade35fa076cb43f69c45de7f7ce57aeb4e5e4d3
topiasp <topias.pyykkonen@gmail.com>
2019-08-19 19:11:51 +0300

    no homepage

--- projects/mplus-output-viewer/service-worker.js
+++ projects/mplus-output-viewer/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projects/mplus-output-viewer/precache-manifest.9ce6125015eb98f2a6f648aededd089b.js"
+  "/precache-manifest.34216ee98f270a58fc0422a9faaac7a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/mplus-output-viewer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
