51fb021e212073031599a4535b1ad0d99640797c
topiasp <topias.pyykkonen@gmail.com>
2019-08-19 20:05:53 +0300

    homepage to package

--- projects/mplus-output-viewer/service-worker.js
+++ projects/mplus-output-viewer/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb582874ffb1fe4188c8f98301bf57f7.js"
+  "/projects/mplus-output-viewer/precache-manifest.13eea11d3e24e4005e6fdb962c567776.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/mplus-output-viewer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
